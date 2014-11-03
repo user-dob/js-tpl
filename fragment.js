e7df19c5f771b4a9c4dbb090d1d005d9cccea193
user-dob <user-dob@yandex.ua>
2014-11-03 17:25:42 +0200

    if

--- fragment.js
+++ fragment.js
@@ -6,8 +6,8 @@ var data = {
     name: 'Rem'
   },
   users: [
-    {name: 'Name 1', age: 14},
-    {name: 'Name 2', age: 14},
+    {name: 'Name 1', age: 14, posts: [{title: 'post 1'}, {title: 'post 2'}, {title: 'post 3'}]},
+    {name: 'Name 2', age: 14, posts: [{title: 'post 1'}, {title: 'post 2'}, {title: 'post 3'}]},
     {name: 'Name 3', age: 15},
     {name: 'Name 4', age: 24},
     {name: 'Name 4', age: 24},
@@ -18,27 +18,58 @@ var data = {
 
 console.time('tpl')
 var text = document.getElementById('tpl-for').innerHTML;
+text = text.replace(/condition="([\s\S]*?)"/g, function(math, p) {
+    return 'condition="' + escape(p) + '"';
+});
+
+//console.log(text)
+
 var frag = document.createDocumentFragment();
 
-var parser = new DOMParser();
-var xml = parser.parseFromString(text, 'application/xml');
+var xml = (new DOMParser()).parseFromString(text, 'application/xml');
+
+//console.log(xml)
+
 var node = xml.documentElement;
 parseTemplate(frag, node, data)
 
 function parseTemplate(frag, node, data) {
-  var nodes = node.childNodes;
-  for(var i=0; i<nodes.length; i++) {
-    var node = nodes[i];
-    switch(node.tagName) {
-      case 'for':
-        parseFor(frag, node, data);
-        break;
+    var nodes = node.childNodes;
+    for(var i=0; i<nodes.length; i++) {
+        parseNode(frag, nodes[i], data);
+    };
+}
 
-      default:
-        parseEl(frag, node, data)
-        break;
+function parseNode(frag, node, data) {
+    switch(node.tagName) {
+        case 'if':
+            parseIf(frag, node, data);
+            break;
+
+        case 'for':
+            parseFor(frag, node, data);
+            break;
+
+        default:
+            parseEl(frag, node, data)
+            break;
     }
-  };
+}
+
+function parseIf(frag, node, data) {
+    var condition = node.getAttribute('condition');
+    condition = unescape(condition);
+    condition = condition.replace(/\{([^{}]*)\}/g, 'data.$1');
+
+    //console.log(condition)
+
+    condition = eval(condition);
+
+    //console.log(condition)
+
+    if(condition) {
+        parseTemplate(frag, node, data);
+    }
 }
 
 function parseFor(frag, node, data) {
@@ -46,17 +77,19 @@ function parseFor(frag, node, data) {
     nodes = node.childNodes,
     itemName, itemsName, o = {};
 
-  condition.replace(/\s*(\w+)\sin\s(\w+)\s*/g, function(math, item, items) {
+  condition = unescape(condition);
+
+  condition.replace(/\s*(\w+)\sin\s([\w\.]+)\s*/g, function(math, item, items) {
     itemName = item;
     itemsName = items;
   });
 
-  if(data[itemsName]) {
-    data[itemsName].forEach(function(item) {
-      o[itemName] = item;
-      for(var i=0; i<nodes.length; i++) {
-        parseEl(frag, nodes[i], o);
-      }
+    data = get(data, itemsName);
+
+    if(data) {
+        data.forEach(function(item) {
+        o[itemName] = item;
+        parseTemplate(frag, node, o);
     });
   }
 }
